2bde58004a21b69c2befedb505961e2f710eb98c
r3natg <renatogava415@gmail.com>
2026-03-11 12:10:07 +0200

    first commit

---
+++ Tervehdys.jsx
@@ -0,0 +1,5 @@
+function Tervehdys({ nimi }) {
+  return <h1>Hei, {nimi}!</h1>;
+}
+
+export default Tervehdys;
---
+++ app.jsx
@@ -0,0 +1,11 @@
+import Tervehdys from "./Tervehdys";
+
+function App() {
+  return (
+    <div>
+      <Tervehdys nimi="Renato" />
+    </div>
+  );
+}
+
+export default App;
