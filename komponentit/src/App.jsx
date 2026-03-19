e3b8379e69c43784d97cc72680fd2f48d0a18394
r3natg <renatogava415@gmail.com>
2026-03-19 11:38:19 +0200

    palautus

--- komponentit/src/App.jsx
+++ komponentit/src/App.jsx
@@ -2,9 +2,16 @@ import Tervehdys from "./Tervehdys";
 import OpiskelijaTiedot from "./OpiskelijaTiedot";
 
 function App() {
+  const opiskelija = {
+    nimi: "Matti Meikäläinen",
+    ika: 16,
+    kurssi: "Reactin perusteet",
+  };
+
   return (
     <div>
       <Tervehdys nimi="Renato" />
+      <OpiskelijaTiedot opiskelija={opiskelija} />
     </div>
   );
 }
